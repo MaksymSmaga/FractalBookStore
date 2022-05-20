5a4aaa9d56df4445b233e3a0ee6652197f6dc615
MaksymSmaga <94846715+MaksymSmaga@users.noreply.github.com>
2022-05-20 23:22:47 +0300

    To fix book cshtml

--- FractalBookStore.Memory/MockBookRepository.cs
+++ FractalBookStore.Memory/MockBookRepository.cs
@@ -11,18 +11,23 @@ namespace FractalBookStore.Memory
         // Array of test data.
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12345-12340", "Alex Jonson","Fractals: a very short introduction", "Fractals: a very...", 13.19m),
-            new Book(2, "ISBN 12345-12341", "Max Steveson","The Fractal Geometry Of Nature", "The Fractal Geometry...", 19.13m),
-            new Book(3, "ISBN 12345-12342", "William Richardson","The Colors Of Infinity", "The Colors...", 13.13m),
-            new Book(4, "ISBN 12345-12343", "John Holly","Fractal Nature Of Life", "Fractal Nature...", 19.19m),
-            new Book(5, "ISBN 12345-12344", "Steve Rest","Fractal And Multifractals", "Fractal And Multifractals...", 113.19m),
-            new Book(6, "ISBN 12345-12345", "Jack Hardy","Introduction To Fractal Theory", "Introduction To Fractal Theory...", 119.13m),
-            new Book(7, "ISBN 12345-12346", "Mark Jackson","Fractals In Geomechanics", "Fractals In Geomechanics...", 113.13m),
-            new Book(8, "ISBN 12345-12347", "Carl Lotty","Fractals Phisics of Self-Similarity", "Fractals Phisics...", 119.19m),
-            new Book(9, "ISBN 12345-12348", "Mark Departh","Beauty Of Fractals", "Beauty Of Fractals...", 3.9m),
-            new Book(10, "ISBN 12345-12349", "Tracy Horny","Basics Of Fractal Geometry", "Basics Of Fractal...", 3.3m),
-            new Book(11, "ISBN 12345-12350", "Anton Galitch","Just A Fractal", "Just A Fractal...", 9.3m),
-            new Book(12, "ISBN 12345-12351", "Peter Garryson","Fractal Between Myth And Craft", "Fractal Between Myth...", 9.9m),
+            new Book(1, "ISBN 12345-12340", "Fractals: a very short introduction", "Alex Jonson",
+                        "VERY SHORT INTRODUCTIONS are for anyone wanting a stimulating and accessible way in to a new"
+                        +"subject. They are written by experts, and have been published in more than 25 languages worldwide."
+                        +"The series began in 1995, and now represents a wide variety of topics in history, philosophy, religion,"
+                        +"science, and the humanities.", 13.19m),
+
+            new Book(2, "ISBN 12345-12341", "The Fractal Geometry Of Nature", "Max Steveson", "The Fractal Geometry...", 19.13m),
+            new Book(3, "ISBN 12345-12342", "The Colors Of Infinity", "William Richardson", "The Colors...", 13.13m),
+            new Book(4, "ISBN 12345-12343", "Fractal Nature Of Life", "John Holly", "Fractal Nature...", 19.19m),
+            new Book(5, "ISBN 12345-12344", "Fractal And Multifractals","Steve Rest", "Fractal And Multifractals...", 113.19m),
+            new Book(6, "ISBN 12345-12345", "Introduction To Fractal Theory", "Jack Hardy", "Introduction To Fractal Theory...", 119.13m),
+            new Book(7, "ISBN 12345-12346", "Fractals In Geomechanics", "Mark Jackson", "Fractals In Geomechanics...", 113.13m),
+            new Book(8, "ISBN 12345-12347", "Fractals Phisics of Self-Similarity", "Carl Lotty", "Fractals Phisics...", 119.19m),
+            new Book(9, "ISBN 12345-12348", "Beauty Of Fractals", "Mark Departh", "Beauty Of Fractals...", 3.9m),
+            new Book(10, "ISBN 12345-12349", "Basics Of Fractal Geometry", "Tracy Horny", "Basics Of Fractal...", 3.3m),
+            new Book(11, "ISBN 12345-12350", "Just A Fractal", "Anton Galitch", "Just A Fractal...", 9.3m),
+            new Book(12, "ISBN 12345-12351", "Fractal Between Myth And Craft", "Peter Garryson", "Fractal Between Myth...", 9.9m),
         };
         public Book[] GetAllByIsbn(string isbn)
         {
@@ -34,12 +39,12 @@ namespace FractalBookStore.Memory
         /// <param name="titlePart"></param>
         /// <returns>Array Book[]</returns>
         public Book[] GetAllByTitleOrAuthor(string query)
-        { 
+        {
             return books.Where(book => book.Author.Contains(query)
                                     || book.Author.Contains(query))
                                     .ToArray();
         }
- public Book GetById(int id)
+        public Book GetById(int id)
         {
             return books.Single(book => book.Id == id);
         }
