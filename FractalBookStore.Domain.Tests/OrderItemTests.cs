5d06b2914984c2cae105f06f390dfec46b7c5e89
MaksymSmaga <94846715+MaksymSmaga@users.noreply.github.com>
2022-05-20 03:57:04 +0300

    To add order, orderItems and tests.

---
+++ FractalBookStore.Domain.Tests/OrderItemTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace FractalBookStore.Domain.Tests
+{
+    public class OrderItemTests
+    {
+        [Fact]
+        public void OrderItem_WithZeroCount_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                int count = -1;
+                new OrderItem(1, count, 0m);
+            });
+        }
+
+        [Fact]
+        public void OrderItem_WithPozitiveCount_SetsCount()
+        {
+            var orderItem = new OrderItem(1, 2, 3m);
+
+            Assert.Equal(1, orderItem.BookId);
+            Assert.Equal(2, orderItem.Count);
+            Assert.Equal(3, orderItem.Price);
+        }
+    }
+}
+
---
+++ FractalBookStore/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FractalBookStore
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        private List<OrderItem> _items;
+
+        public IReadOnlyCollection<OrderItem> Items
+        {
+            get { return _items; }
+        }
+
+        public int TotalCount
+        {
+            get { return _items.Sum(item => item.Count); }
+        }
+
+        public decimal TotalPrice
+        {
+            get { return _items.Sum(item => item.Price * item.Count); }
+        }
+
+        public Order(int id, IEnumerable<OrderItem> items)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            Id = id;
+
+            _items = new List<OrderItem>(items);
+        }
+    }
+}
