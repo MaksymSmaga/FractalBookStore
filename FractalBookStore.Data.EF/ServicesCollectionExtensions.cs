80f9624e608111bd4048849a589c5763b36dab06
MaksymSmaga <94846715+MaksymSmaga@users.noreply.github.com>
2022-05-27 01:02:02 +0300

    To Add-Migration InitialCreate -Project FractalBookStore.Data.EF -StartupProject FractalBookStore.Web

---
+++ FractalBookStore.Data.EF/ServicesCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FractalBookStore.Data.EF
+{
+    public static class ServicesCollectionExtensions
+    {
+        public static IServiceCollection AddEFRepository(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<StoreDBContext>(
+                options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+
+            return services;
+        }
+    }
+}
